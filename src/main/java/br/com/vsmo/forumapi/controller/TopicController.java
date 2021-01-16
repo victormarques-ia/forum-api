2aba3c3a6d300d94b2198a3184328dfc68ba286c
Victor Silva <victormarques.ia@gmail.com>
2021-01-16 12:42:47 -0300

    feat: add create method on TopicController

--- src/main/java/br/com/vsmo/forumapi/controller/TopicController.java
+++ src/main/java/br/com/vsmo/forumapi/controller/TopicController.java
@@ -1,23 +1,35 @@
 package br.com.vsmo.forumapi.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.vsmo.forumapi.controller.dtos.TopicDTO;
+import br.com.vsmo.forumapi.controller.form.TopicForm;
 
 import br.com.vsmo.forumapi.model.Topic;
+import br.com.vsmo.forumapi.repositories.CourseRepository;
 import br.com.vsmo.forumapi.repositories.TopicRepository;
 
 @RestController
+@RequestMapping("/topics")
 public class TopicController {
 
   @Autowired
   private TopicRepository topicRepository;
 
-  @RequestMapping("/topics")
+  @Autowired
+  private CourseRepository courseRepository;
+
+  @GetMapping
   public List<TopicDTO> listTopics(String courseName) {
     if (courseName == null) {
       List<Topic> topics = topicRepository.findAll();
@@ -30,4 +42,13 @@ public class TopicController {
     }
   }
 
+  @PostMapping
+  public ResponseEntity<TopicDTO> createTopic(@RequestBody TopicForm topicForm, UriComponentsBuilder uriBuilder) {
+    Topic topic = topicForm.mapper(courseRepository);
+    topicRepository.save(topic);
+
+    URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
+
+    return ResponseEntity.created(uri).body(new TopicDTO(topic));
+  }
 }
