17d5ea516d053f0d352be4861217de204070e660
Victor Silva <victormarques.ia@gmail.com>
2021-01-17 08:54:59 -0300

    feat: create update and delete methods on TopicController

--- src/main/java/br/com/vsmo/forumapi/controller/TopicController.java
+++ src/main/java/br/com/vsmo/forumapi/controller/TopicController.java
@@ -3,20 +3,25 @@ package br.com.vsmo.forumapi.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.vsmo.forumapi.controller.dtos.TopicDTO;
+import br.com.vsmo.forumapi.controller.dtos.TopicDetailsDTO;
 import br.com.vsmo.forumapi.controller.form.TopicForm;
-
+import br.com.vsmo.forumapi.controller.form.TopicUpdateForm;
 import br.com.vsmo.forumapi.model.Topic;
 import br.com.vsmo.forumapi.repositories.CourseRepository;
 import br.com.vsmo.forumapi.repositories.TopicRepository;
@@ -32,7 +37,7 @@ public class TopicController {
   private CourseRepository courseRepository;
 
   @GetMapping
-  public List<TopicDTO> listTopics(String courseName) {
+  public List<TopicDTO> findTopics(String courseName) {
     if (courseName == null) {
       List<Topic> topics = topicRepository.findAll();
 
@@ -45,6 +50,7 @@ public class TopicController {
   }
 
   @PostMapping
+  @Transactional
   public ResponseEntity<TopicDTO> createTopic(@RequestBody @Valid TopicForm topicForm,
       UriComponentsBuilder uriBuilder) {
     Topic topic = topicForm.mapper(courseRepository);
@@ -53,5 +59,28 @@ public class TopicController {
     URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
 
     return ResponseEntity.created(uri).body(new TopicDTO(topic));
+  }
+
+  @GetMapping("/{id}")
+  public TopicDetailsDTO findTopic(@PathVariable Long id) {
+    Topic topic = topicRepository.getOne(id);
+    return new TopicDetailsDTO(topic);
+  }
+
+  @PutMapping("/{id}")
+  @Transactional
+  public ResponseEntity<TopicDTO> updateTopic(@PathVariable Long id,
+      @RequestBody @Valid TopicUpdateForm topicUpdateForm) {
+    Topic topic = topicUpdateForm.update(id, topicRepository);
+
+    return ResponseEntity.ok(new TopicDTO(topic));
+  }
+
+  @DeleteMapping("/{id}")
+  @Transactional
+  public ResponseEntity removeTopic(@PathVariable Long id) {
+    topicRepository.deleteById(id);
+
+    return ResponseEntity.ok().build();
   }
 }
