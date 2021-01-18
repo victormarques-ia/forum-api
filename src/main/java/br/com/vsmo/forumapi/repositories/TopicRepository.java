949e31112e09bede7f0c180e39105d324e2d32b8
Victor Silva <victormarques.ia@gmail.com>
2021-01-18 20:29:48 -0300

    refactor: change type List to Page on TopicRepository findByCourseName method

--- src/main/java/br/com/vsmo/forumapi/repositories/TopicRepository.java
+++ src/main/java/br/com/vsmo/forumapi/repositories/TopicRepository.java
@@ -1,13 +1,13 @@
 package br.com.vsmo.forumapi.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.vsmo.forumapi.model.Topic;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
-  List<Topic> findByCourseName(String courseName);
+  Page<Topic> findByCourseName(String courseName, Pageable pagination);
 
 }
