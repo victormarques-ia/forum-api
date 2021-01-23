06643c69ccc17361c0399c198c430d347393eace
Victor Silva <victormarques.ia@gmail.com>
2021-01-23 07:55:14 -0300

    refactor: changes on SecurityConfigurations

--- src/main/java/br/com/vsmo/forumapi/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/vsmo/forumapi/config/security/SecurityConfigurations.java
@@ -1,13 +1,16 @@
 package br.com.vsmo.forumapi.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -16,6 +19,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
   @Autowired
   private AuthenticationService authenticationService;
+
+  // Configurações para gerenciar a autenticação
+  @Override
+  @Bean
+  protected AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager();
+  }
 
   // Configurações de autenticação
   @Override
@@ -26,8 +36,14 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
   // Configurações de autorização
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers(HttpMethod.GET, "/topics").permitAll().antMatchers(HttpMethod.GET, "topics/*")
-        .permitAll().anyRequest().authenticated().and().formLogin();
+    http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/topics").permitAll()
+            .antMatchers(HttpMethod.GET, "/topics/*")
+            .permitAll()
+            .antMatchers(HttpMethod.POST, "/auth")
+            .permitAll()
+            .anyRequest().authenticated().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
   }
 
   // Configurações de recursos estáticos(imagens...)
