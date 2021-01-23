8d55db16079236476546b8ef0199ca9dcd6b3a31
Victor Silva <victormarques.ia@gmail.com>
2021-01-23 07:55:29 -0300

    feat: create AuthenticationController

---
+++ src/main/java/br/com/vsmo/forumapi/controller/AuthenticationController.java
@@ -0,0 +1,44 @@
+package br.com.vsmo.forumapi.controller;
+
+import br.com.vsmo.forumapi.config.security.TokenService;
+import br.com.vsmo.forumapi.controller.dtos.TokenDTO;
+import br.com.vsmo.forumapi.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+  @Autowired
+  private AuthenticationManager authManager;
+
+  @Autowired
+  private TokenService tokenService;
+
+  @PostMapping
+  public ResponseEntity<TokenDTO> authenticate(@RequestBody @Valid LoginForm loginForm) {
+    UsernamePasswordAuthenticationToken loginData = loginForm.mapper();
+    try {
+      Authentication authentication = authManager.authenticate(loginData);
+
+      String token = tokenService.generateToken(authentication);
+
+      return ResponseEntity.ok(new TokenDTO(token, "Bearer"));
+    } catch (AuthenticationException e) {
+      return ResponseEntity.badRequest().build();
+    }
+
+  }
+
+}
